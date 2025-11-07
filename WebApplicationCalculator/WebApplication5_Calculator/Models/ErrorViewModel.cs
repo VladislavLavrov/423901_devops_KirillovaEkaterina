23af46a4d38cae1e65cfd6bc026d6ffeb2ce34ee
ecattrina <katy_060104@mail.ru>
2025-11-07 15:09:03 +0500

    Удаление старого приложения

--- WebApplicationCalculator/WebApplication5_Calculator/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication5_Calculator.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
